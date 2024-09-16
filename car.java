abf0a6b305411a6fc01ea1f443bed4cc38847cd8
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-09-16 21:20:51 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,20 @@
+public class car {
+
+    String make = "Ford";
+    String model = "mustang gt";
+    int year = 2022;
+    double price = 50000.00;
+
+    static void drive()
+    {
+        System.out.println("u drive a mustang gt 2022 model");
+    }
+
+    static void brake()
+    {
+        System.out.println("u step on the break");
+    }
+
+
+
+}
