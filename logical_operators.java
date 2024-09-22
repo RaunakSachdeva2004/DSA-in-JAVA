f7bfff2fd905722510cd6a513393567ac44db786
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-09-22 19:10:07 +0530

    Add files via upload

---
+++ logical_operators.java
@@ -0,0 +1,23 @@
+public class logical_operators {
+        public static void main(String[] args) {
+        
+            // && and operator
+            // || OR
+            // ! NOT
+
+            int temp = 25;
+
+            if (temp>30){
+                System.out.println("It is hot outsid");
+            }
+
+            else if (temp>=20 && temp<=30){
+                System.out.println("it is warm outside.");
+
+
+            }
+            else {
+                System.out.println("it is cold.");
+            }
+    }
+}
