fac69ffa78627f1bd55221575792ca634527dcd5
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-09-23 20:25:51 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,33 @@
+public class methods {
+    
+    /*public static void main(String[] args) {
+        //method = is a block of code that is executed when called
+        String name = "raunak ";
+        int age = 21;
+          hello(name,age);
+
+    }
+
+    static void hello(String name,int age){
+
+        System.out.println("hello "+name);
+        System.out.println("my age is "+age);
+    }*/
+
+
+    public static void main(String[] args) {
+        
+        int x = 3;
+        int y = 4;
+
+        System.out.println(add(x,y));
+
+    }
+
+    static int add(int x, int y) {
+
+        return x + y;
+    }
+
+
+}
