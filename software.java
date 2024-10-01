0ef4c316579d5c4f47e04ead26fa7ad50824a1cd
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-10-01 09:04:53 +0530

    Add files via upload

---
+++ software.java
@@ -0,0 +1,14 @@
+public class software{
+    String sw_name;
+    int sw_id;
+     public void setdata( String name, int id){
+        this.sw_name=name;
+        this.sw_id=id;
+    }
+     public void getdata(){
+      System.out.println(sw_name);
+      System.out.println(sw_id);
+    }    }
+
+
+    
