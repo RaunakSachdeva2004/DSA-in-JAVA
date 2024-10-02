a5d7d823a3cbc06e6b7563e30971e01695f026ea
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-10-02 11:02:30 +0530

    Add files via upload

---
+++ String_methods.java
@@ -0,0 +1,21 @@
+public class String_methods {
+    public static void main(String[] args) {
+        
+        String name = "    Raunak     ";
+
+        // boolean result = name.equalsIgnoreCase("s");
+        //int result = name.length();
+        //char result = name.charAt(5);
+        //int result = name.indexOf("n");
+        //boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.trim();
+        String result = name.replace("R", "S");
+        
+        
+        
+        System.out.println(result);
+
+    }
+}
