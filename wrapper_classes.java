850ae904e357063b48dfa64f7164184c63bba633
Raunak Sachdeva <152476869+RaunakSachdeva2004@users.noreply.github.com>
2024-10-14 20:51:01 +0530

    Add files via upload

---
+++ wrapper_classes.java
@@ -0,0 +1,26 @@
+public class wrapper_classes {
+    public static void main(String[] args) {
+        
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String s = "Raunak";
+
+        
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+        
+        
+        if (b == '@') {
+            
+            System.out.println("it is true");
+            
+        }
+    }
+}
